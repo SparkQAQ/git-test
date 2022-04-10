50187edea932772f795edceb492522bf52072275
SPARK <1791832600@qq.com>
2022-04-10 15:09:03 +0800

    hot-fix commit1

--- src/main/java/com/atguigu/git_test.java
+++ src/main/java/com/atguigu/git_test.java
@@ -5,5 +5,6 @@ public class git_test {
         System.out.println("hello,git");
         System.out.println("hello,git 2");
         System.out.println("hello,git 3");
+        System.out.println("hot -fix -1");
     }
 }
